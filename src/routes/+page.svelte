7e8ef2dcf8ddc5f572973b3a4a66966615a6e6c7
William Fairclotb <willuhmjs@gmail.com>
2023-04-21 18:56:05 +0000

    transfer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,8 +4,12 @@
     $: url = `${window.location.protocol}//${window.location.host}/${window.btoa(new URLSearchParams(Object.fromEntries(Object.entries($params).filter(([_, v]) => v !== ''))).toString())}`
 </script>
 <main>
+
 <section>
+  <div class="header"><h1>rickhider</h1></div>
   <Inputs/>
+  <button on:click={() => alert("hi")}>Shorten Link</button>
+
   </section>
   <section>
     <!-- preview of meta tag => embedded rickroll URL -->
@@ -36,5 +40,34 @@
     flex-direction: column;
     margin: 1rem;
   }
+}
+
+button {
+  padding: 0.8rem;
+  border: 0;
+  font-weight: bold;
+  width: 100%;
+  font-size: 20px;
+  border-radius: 5px;
+  background-image: url("https://cdn.vox-cdn.com/thumbor/Si2spWe-6jYnWh8roDPVRV7izC4=/0x0:1192x795/1400x788/filters:focal(596x398:597x399)/cdn.vox-cdn.com/uploads/chorus_asset/file/22312759/rickroll_4k.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position-y: top;
+  margin: 1rem 0;
+}
+
+h1 {
+  text-align: center;
+  font-size: 40px;
+}
+
+.header {
+  background-clip: text;
+  background-image: url("https://cdn.vox-cdn.com/thumbor/Si2spWe-6jYnWh8roDPVRV7izC4=/0x0:1192x795/1400x788/filters:focal(596x398:597x399)/cdn.vox-cdn.com/uploads/chorus_asset/file/22312759/rickroll_4k.jpg");
+  -webkit-background-clip: text;
+  color: transparent;
+  background-position-x: center;
+  background-position-y: top;
+
 }
 </style>

8bf93d69f1e963c59f1097001d85f82995d9fe18
willuhmjs <willuhmjs@gmail.com>
2023-04-20 23:15:27 -0400

    chore: delete onmount

--- src/routes/[encoded]/+page.svelte
+++ src/routes/[encoded]/+page.svelte
@@ -1,10 +1,5 @@
 <script>
-	import { onMount } from 'svelte';
-
     export let data;
-    onMount(() => {
-        //window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
-    })
 </script>
 
 <!-- og meta tags -->

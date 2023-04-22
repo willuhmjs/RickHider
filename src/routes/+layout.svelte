0ed77331dbff67416a8841b59d296271e3381588
willuhmjs <willuhmjs@gmail.com>
2023-04-22 16:17:45 -0400

    idk why meta tags arent showing

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,16 @@
 <script>
 	import './style.scss';
 </script>
+<svelte:head>
+	<meta name="title" content="Rickhider - Custom Rickroll Generator" />
+	<meta name="description" content="Rickroll your friends by decieving them with custom metadata." />
+  	<meta property="og:type" content='website' />
+	<meta property="theme-color" content="#843c33" />
+	<meta name="og:title" content="Rickhider - Custom Rickroll Generator" />
+	<meta name="og:description" content="Rickroll your friends by decieving them with custom metadata." />
+	<meta name="twitter:card" content='summary' />
+	<meta name="twitter:title" content="Rickhider - Custom Rickroll Generator" />
+	<meta name="twitter:description" content="Rickroll your friends by decieving them with custom metadata." />
+</svelte:head>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <slot />

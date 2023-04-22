612b80ec2d722a615024652c35784f4de027f121
willuhmjs <willuhmjs@gmail.com>
2023-04-22 14:31:11 -0400

    feat: global styles

--- src/lib/Inputs.svelte
+++ src/lib/Inputs.svelte
@@ -21,25 +21,3 @@
 		</div>
 	{/if}
 {/each}
-
-<style lang="scss">
-	.inputGroup {
-		margin: 0.5rem 0;
-	}
-
-	input,
-	select {
-		width: 100%;
-		padding: 0.8rem;
-		margin-top: 4px;
-		border-radius: 4px;
-		border: 0;
-		background-color: #f5f5f5;
-	}
-
-	label {
-		/* smaller and gray */
-		font-size: 0.8rem;
-		color: #666;
-	}
-</style>
